e2364c919667947b5f2778d1a16c5227aa2cda8e
Romain Dugast <dugast.romain@gmail.com>
2021-06-17 15:53:45 +0200

    nouvelles fonctions intensité

--- src/modules python/fonctions_espace.py
+++ src/modules python/fonctions_espace.py
@@ -4,11 +4,13 @@ import numpy as np
 def OneBump(x,y):
     bump = 0.5-3.*((x-0.5)**2+(y-0.5)**2)
     return np.maximum(bump, np.zeros_like(x))
+
 def ThreeBumps(x,y):
     bump1 = 0.3-3*((x-0.4)**2+(y-0.5)**2)
     bump2 = 0.25-3*((x-0.65)**2+(y-0.6)**2)
     bump3 = 0.25-3*((x-0.6)**2+(y-0.35)**2)
     return np.maximum.reduce([bump1,bump2,bump3,np.zeros_like(bump1)])
+
 def Volcano(x,y):
     r = np.sqrt((x-0.5)**2+(y-0.5)**2)
     volcano = 0.05+1.5*(1+x)*(r**2-6*r**4)
@@ -16,12 +18,14 @@ def Volcano(x,y):
 
 def GenerateRHS(height,params):
     α,β,γ,h = params
-    hx,hy = np.gradient(height,h)
+    hy,hx = np.gradient(height,h)
     Intensity = (α*hx+β*hy+γ)/np.sqrt(1+hx**2+hy**2)
     Omega = height>0
     return Intensity,Omega
+
 def gaussienne_x(x,y) :
     return(np.exp(-7*(x-0.5)**2))
+
 def gaussienne(x,y) :
     return(np.exp(-5*((x-0.5)**2+(y-0.5)**2)))
 
@@ -29,4 +33,46 @@ def parametres(azimuth, elevation) :
     azimuth_rad = np.pi*azimuth/180
     elevation_rad = np.pi*elevation/180
     return(-np.cos(azimuth_rad)*np.cos(elevation_rad), -np.sin(azimuth_rad)*np.cos(elevation_rad),np.sin(elevation_rad))
+
+def GenerateIntDiscont(height,params) :
+    (n,m) = height.shape
+    α,β,γ,h = params
+    hx,hy = np.gradient(height,h)
+    hx[n//2,:] = np.zeros(m)
+    hx[n//2-1,:] = np.zeros(m)
+    Intensity = (α*hx+β*hy+γ)/np.sqrt(1+hx**2+hy**2)
+    Omega = height>0
+    return Intensity,Omega
+
+def GenerateIntIter(U,I,params) :
+    (n,m) = U.shape
+    α,β,γ,h = params
+    p0 = np.array([-α,-β,γ])
+    Intensity = np.zeros((n,m))
+    hy,hx = np.gradient(U,h)
+    max = 0
+    for i in range(n) :
+        for j in range(m) :
+            p = p0*I[i,j]
+            x = np.array([-hx[i,j],-hy[i,j],1])
+            w= x
+            k = x-p-np.dot(x-p,p)*p
+            x = p + np.sqrt(1-np.dot(p,p))*((k)/np.sqrt(np.dot(k,k)))
+            Intensity[i,j] = x[2]
+            if np.dot(x-w,x-w) > max :
+                max = np.dot(x-w,x-w)
+            if max > 31 :
+                pass
+    print(max)
+    return(Intensity)
+
+def IntInit(I,params) :
+    n,m = I.shape
+    I0 = np.zeros((n,m))
+    for i in range(n) :
+        for j in range(m) :
+
+
+
+
 
